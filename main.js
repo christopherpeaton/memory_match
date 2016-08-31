e2fd6a553f000fd23f50356d14edfada725141b8
Christopher Eaton <christopherpeaton@gmail.com>
2016-08-30 18:37:40 -0700

    trying to figure out the click/hide/show

--- main.js
+++ main.js
@@ -1,28 +1,20 @@
-
 var first_card = null;
 var second_card = null;
 var matches = 0;
 
-$(document).ready(function(){
-    $(".card_front").click(function(){
-
-        console.log("doc.ready first card clicked");
-    })
-
-});
-
-if (first_card){
-    $(".first_card").addClass('clicked');
-    console.log('first card clicked');
-} else {
-    $(".second_card").addClass('clicked');
-    console.log('second card clicked');
-        if (first_card === second_card){
-            $(".first_card").addClass('match');
-            $(".second_card").addClass('match');
-            matches += 1;
-            console.log('match');
+$(document).ready(function () {
+    $(".card_back").click(function () {
+        if (first_card === null) {
+            $(this).hide();
+            console.log('first card clicked');
         } else {
+            if (first_card === second_card) {
 
+
+                console.log('match');
+            } else {
+
+            }
         }
-}
+    })
+});
