b38ee3546990edc143468a8f62f06be20bfa3fa8
Christopher Eaton <christopherpeaton@gmail.com>
2016-08-31 16:21:09 -0700

    need to make matches into objects, continue working on getting .show to work

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
 var first_card = null;
 var second_card = null;
+var matched = [];
 var matches = 0;
 
 $(document).ready(function () {
     $(".card_back").click(function () {
         if (first_card === null) {
             $(this).hide();
-            console.log('first card clicked');
+            first_card = $(this).siblings().find('img').attr('src');
+            console.log('first card clicked', first_card);
+
         } else {
+            $(this).hide();
+            second_card = $(this).siblings().find('img').attr('src');
+            console.log('second card clicked', second_card);
             if (first_card === second_card) {
-
-
-                console.log('match');
+                matched.push
+                matches += 1;
             } else {
+                setTimeout(function(){
 
+                    first_card.siblings().find('.card_back').show();
+                    second_card.siblings().find('.card_back').show();
+                    clicked.length = 0;
+                    first_card = null;
+                    second_card = null;
+                }, 1500);
             }
         }
     })
