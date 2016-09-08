8f82f55075e6653b8826055767a2e12bf6ca2174
Christopher Eaton <christopherpeaton@gmail.com>
2016-09-08 16:39:36 -0700

    display stats works, need to add % sign and fine tune accuracy percentage.

--- main.js
+++ main.js
@@ -2,14 +2,20 @@ var first_card = null;
 var second_card = null;
 var matched = [];
 var matches = 0;
+var attempts = 0;
+var accuracy;
+var games_played = 0;
 
-$(document).ready(function () {
+$(document).ready(function (){
+    displayStats();
+
 
     $(".card_back").click(function () {
         if (first_card === null) {
             $(this).hide();
             first_card = $(this).siblings().find('img').attr('src');
             console.log('first card clicked', first_card);
+            displayStats();
         } else {
             $(this).hide();
             second_card = $(this).siblings().find('img').attr('src');
@@ -22,16 +28,40 @@ $(document).ready(function () {
                 };
                 matched.push(match);
                 matches += 1;
+                attempts += 1;
                 first_card = null;
                 second_card = null;
+                displayStats();
             } else {
                 setTimeout(function () {
                     $("img[src='" + first_card + "']").parent().siblings().show();
                     $("img[src='" + second_card + "']").parent().siblings().show();
                     first_card = null;
                     second_card = null;
+                    attempts += 1;
+                    displayStats();
                 }, 2000);
             }
         }
-    })
+        displayStats();
+    });
+
+
+    function displayStats(){
+        $(".played_value").html(games_played);
+        $(".attempts_value").html(attempts);
+        var accurate = Math.round(matches / attempts);
+        $(".accuracy_value").html(accurate * 100);
+        console.log('accurate :',accurate);
+        $(".matches_value").html(matches);
+    }
+
+    function resetStats(reset_clicked){
+        accuracy = 0;
+        matches = 0;
+        attempts = 0;
+        displayStats();
+    }
+
+
 });
